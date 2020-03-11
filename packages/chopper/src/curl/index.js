3a463e0cb11097d250aa9df7951cd440948d34a9
Newton Koumantzelis <mail@iamnewton.com>
2020-03-11 15:51:22 -0700

    chore: create curl docs

--- packages/chopper/src/curl/index.js
+++ packages/chopper/src/curl/index.js
@@ -11,30 +11,30 @@ const _exec = command =>
 		});
 	});
 
-export const getData = (url, token, params) =>
-	_exec(`curl -H 'Authorization: Bearer ${token}' ${uri.addParams(url, { ...params })}`)
-		.then(stdout => JSON.parse(stdout));
-
 export const getHTTPStatus = (url, token) =>
 	_exec(`curl -H 'Authorization: Bearer ${token}' -s -o /dev/null -I -w '%{http_code}' ${url}`)
 		.then(stdout => JSON.parse(stdout));
 
-export const exists = (url, token) =>
+export const isUp = (url, token) =>
 	getHTTPStatus(url, token)
 		.then(status => !((status >= 400)));
 
-export const postData = (url, token, payload) =>
-	_exec(`curl -X POST -H 'Authorization: Bearer ${token}' -H 'Content-Type: application/json' -d '${JSON.stringify(payload)}' ${url}`)
+export const getData = (url, token, params) =>
+	_exec(`curl -H 'Authorization: Bearer ${token}' ${uri.addParams(url, { ...params })}`)
 		.then(stdout => JSON.parse(stdout));
 
 export const patchData = (url, token, payload) =>
 	_exec(`curl -X PATCH -H 'Authorization: Bearer ${token}' -H 'Content-Type: application/json' -d '${JSON.stringify(payload)}' ${url}`)
 		.then(stdout => JSON.parse(stdout));
 
+export const postData = (url, token, payload) =>
+	_exec(`curl -X POST -H 'Authorization: Bearer ${token}' -H 'Content-Type: application/json' -d '${JSON.stringify(payload)}' ${url}`)
+		.then(stdout => JSON.parse(stdout));
+
 export default {
 	get: getData,
 	getHTTPStatus,
-	exists,
-	post: postData,
+	isUp,
 	patch: patchData,
+	post: postData,
 };
