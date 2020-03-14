326fe487178931996685d1a63cac7ac6026be0ea
Newton Koumantzelis <mail@iamnewton.com>
2020-03-13 20:34:13 -0700

    feat: initial animation lib

---
+++ packages/arthree/test/true.setup.js
@@ -0,0 +1,14 @@
+/* global describe it */
+/* eslint no-undef: "error" */
+const { join } = require('path');
+const { runSass } = require('sass-true');
+const sass = require('sass');
+const loadPath = [
+    'node_modules',
+];
+
+const file = join(__dirname, 'test.scss');
+runSass({ file, loadPath }, { describe, it, sass });
+
+const fileWithConfig = join(__dirname, 'test-with-configuration.scss');
+runSass({ file: fileWithConfig, loadPath }, { describe, it, sass });
